e603dbff64e7e630197efe1250482424d6eec841
Saul Greenberg <saul@ucalgary.ca>
2019-06-25 19:09:31 -0600

    Verson number to 2.2.2.7

--- Timelapse2TemplateEditor/TimelapseTemplateEditor/Properties/AssemblyInfo.cs
+++ Timelapse2TemplateEditor/TimelapseTemplateEditor/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.2.2.5")]
-[assembly: AssemblyFileVersion("2.2.2.5")]
+[assembly: AssemblyVersion("2.2.2.7")]
+[assembly: AssemblyFileVersion("2.2.2.7")]
 
 [assembly: InternalsVisibleTo("Timelapse2.UnitTests")]
