b4622f0b01c9e35f270745ebeb44b94737246fd0
saulgreenberg <saul@ucalgary.ca>
2019-12-15 14:25:20 -0700

    Comments added. Populate metadata... I thought it was an error, but it appears that Quicktime mp4s can have multiple metadate directories called Quicktime Track Headers. While Exiftool seems to handle this properly, MetadataDetector generates duplicates. I noted this in code as a comment

--- Timelapse2/Timelapse/Util/ImageMetadataDictionary.cs
+++ Timelapse2/Timelapse/Util/ImageMetadataDictionary.cs
@@ -13,22 +13,30 @@ namespace Timelapse.Util
         public static Dictionary<string, ImageMetadata> LoadMetadata(string filePath)
         {
             Dictionary<string, ImageMetadata> metadataDictionary = new Dictionary<string, ImageMetadata>();
+
+            var foo = ImageMetadataReader.ReadMetadata(filePath);
+
             try
             {
                 foreach (Directory metadataDirectory in ImageMetadataReader.ReadMetadata(filePath))
                 {
+                    // TraceDebug.PrintMessage(String.Format("metadataDirectory is: {0}", metadataDirectory.Name));
                     foreach (Tag metadataTag in metadataDirectory.Tags)
                     {
                         ImageMetadata metadata = new ImageMetadata(metadataTag.DirectoryName, metadataTag.Name, metadataTag.Description);
+
                         // Check if the metadata name is already in the dictionary.
                         // If so, just skip it as its not clear what else to do with it
+                        // Note that Quicktime mp4s appear to have multiple directories called Quicktime Track Headers. 
+                        // Exiftool seems to handle this properly but MetadataDetector generates duplicates.
+                        // So only the first Quicktime track header is added to the dictionary.
                         if (!metadataDictionary.ContainsKey(metadata.Key))
                         {
                             metadataDictionary.Add(metadata.Key, metadata);
                         }
                         else
                         {
-                            TraceDebug.PrintMessage(String.Format("ImageMetadata Dictionary: Duplicate metadata key: {0}", metadata.Key));
+                            TraceDebug.PrintMessage(String.Format("ImageMetadata Dictionary: Duplicate metadata key: {0}:{1} (Note that Quicktime may have multiple Track Headers)", metadataDirectory.Name, metadata.Key));
                         }
                     }
                 }
