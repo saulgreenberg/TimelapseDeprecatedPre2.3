539e309659915175cc4a05e2fa95f28a19a6c875
saulgreenberg <saul@ucalgary.ca>
2022-03-02 09:13:56 -0700

    CreateTable - rare bug fix where it now tests if the table exists before trying to create it.

--- Timelapse2/Timelapse/Database/SQLiteWrapper.cs
+++ Timelapse2/Timelapse/Database/SQLiteWrapper.cs
@@ -59,6 +59,12 @@ namespace Timelapse.Database
             // Check the arguments for null 
             ThrowIf.IsNullArgument(columnDefinitions, nameof(columnDefinitions));
 
+            if (this.TableExists(tableName))
+            {
+                // Just in case the table exists, we will want to remove it before trying to create it.
+                this.DropTable(tableName);
+            }
+
             string query = Sql.CreateTable + tableName + Sql.OpenParenthesis + Environment.NewLine;               // CREATE TABLE <tablename> (
             foreach (SchemaColumnDefinition column in columnDefinitions)
             {
